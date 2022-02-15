40b677a1ee4a2b8c307d30ce5bc11aa8d0d4109d
박예빈 <codepark.kr@gmail.com>
2022-02-15 14:33:05 +0900

    [CFG] Project Initialize

---
+++ backend/src/main/java/com/dc/dropthecode/DropTheCodeApplication.java
@@ -0,0 +1,13 @@
+package com.dc.dropthecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropTheCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropTheCodeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/dc/dropthecode/DropTheCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.dropthecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropTheCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
