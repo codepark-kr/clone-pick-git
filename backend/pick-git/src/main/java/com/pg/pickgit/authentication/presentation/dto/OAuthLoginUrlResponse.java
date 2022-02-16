c2025cb52a7eaf318515b3e1906eda88940b7475
박예빈 <codepark.kr@gmail.com>
2022-02-16 16:36:57 +0900

    [ADD] Add DTOs for present authentication
    - Newly add OAuthLoginUrlResponse,
      OAuthTokenResponse

---
+++ backend/pick-git/src/main/java/com/pg/pickgit/authentication/presentation/dto/OAuthLoginUrlResponse.java
@@ -0,0 +1,14 @@
+package com.pg.pickgit.authentication.presentation.dto;
+
+public class OAuthLoginUrlResponse {
+
+    private String url;
+
+    private OAuthLoginUrlResponse(){}
+
+    public OAuthLoginUrlResponse(String url){ this.url = url; }
+
+    public String getUrl(){ return url; }
+
+    public void setUrl(String url){ this.url = url; }
+}
---
+++ backend/pick-git/src/main/java/com/pg/pickgit/authentication/presentation/dto/OAuthTokenResponse.java
@@ -0,0 +1,19 @@
+package com.pg.pickgit.authentication.presentation.dto;
+
+public class OAuthTokenResponse {
+
+    private String token;
+    private String username;
+
+    private OAuthTokenResponse(){}
+
+    public OAuthTokenResponse(String token, String username){
+        this.token = token;
+        this.username = username;
+    }
+
+    public String getToken(){ return token; }
+
+    public String getUsername(){ return username; }
+
+}
