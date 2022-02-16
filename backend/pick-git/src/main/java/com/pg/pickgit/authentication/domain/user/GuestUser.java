dcd0e0159cd661b9259ba8aab578c77a7d3488af
박예빈 <codepark.kr@gmail.com>
2022-02-16 16:32:15 +0900

    [ADD] Add new DTO about user with authorization
    - Newly add App User,
      LoginUser,
      GuestUser

---
+++ backend/pick-git/src/main/java/com/pg/pickgit/authentication/domain/user/GuestUser.java
@@ -0,0 +1,20 @@
+package com.pg.pickgit.authentication.domain.user;
+
+import com.pg.pickgit.exception.authentication.UnauthorizedException;
+
+public class GuestUser extends AppUser{
+
+    private static final String DUMMY_USERNAME = "anonymous";
+    private static final String DUMMY_ACCESSTOKEN = "anonymous";
+
+    public GuestUser(){ super(DUMMY_USERNAME, DUMMY_ACCESSTOKEN); }
+
+    @Override
+    public boolean isGuest() { return true; }
+
+    @Override
+    public String getUsername() { throw new UnauthorizedException(); }
+
+    @Override
+    public String getAccessToken() { throw new UnauthorizedException(); }
+}
---
+++ backend/pick-git/src/main/java/com/pg/pickgit/authentication/domain/user/LoginUser.java
@@ -0,0 +1,9 @@
+package com.pg.pickgit.authentication.domain.user;
+
+public class LoginUser extends AppUser{
+
+    public LoginUser(String username, String accessToken){ super(username, accessToken); }
+
+    @Override
+    public boolean isGuest() { return false; }
+}
