d3af8a68950142b0f194913acb15ee8c3d50c638
박예빈 <codepark.kr@gmail.com>
2022-02-16 12:50:25 +0900

    [ADD] Add new ApiDocumentUtils in Test

---
+++ backend/pick-git/src/test/java/com/pg/pickgit/docs/ApiDocumentUtils.java
@@ -0,0 +1,13 @@
+package com.pg.pickgit.docs;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public interface ApiDocumentUtils {
+
+    static OperationRequestPreprocessor getDocumentRequest(){ return preprocessRequest(prettyPrint()); }
+
+    static OperationResponsePreprocessor getDocumentResponse(){ return preprocessResponse(prettyPrint()); }
+}
