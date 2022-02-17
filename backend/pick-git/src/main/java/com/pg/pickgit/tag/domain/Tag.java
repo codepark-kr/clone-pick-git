75dfbd3118d9b7e009d97b5382c1ea73cb7975e7
박예빈 <codepark.kr@gmail.com>
2022-02-17 17:20:57 +0900

    [ADD] Add new domain/tag
    - Newly add new domains :
      Tag(Single - tag),
      PlatformTagExtractor,
      TagRepository

---
+++ backend/pick-git/src/main/java/com/pg/pickgit/tag/domain/Tag.java
@@ -0,0 +1,47 @@
+package com.pg.pickgit.tag.domain;
+
+import javax.persistence.*;
+import java.util.Locale;
+import java.util.Objects;
+
+@Entity
+public class Tag {
+
+    private static final int MAX_TAG_LENGTH = 20;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = MAX_TAG_LENGTH)
+    private String name;
+
+    protected Tag(){}
+
+    public Tag(String name){
+        if(isNotValidTag(name)){ throw new TagFormatException(); }
+        this.name = name.toLowerCase();
+    }
+
+    private boolean isNotValidTag(String name){
+        return Objects.isNull(name)
+                || name.isBlank()
+                || name.length() > MAX_TAG_LENGTH;
+    }
+
+    public Long getId(){ return id; }
+
+    public String getName(){ return name; }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o){ return true; }
+        if(!(o instanceof Tag)){ return false; }
+
+        Tag tag = (Tag) o;
+        return Objects.equals(name, tag.getName());
+    }
+
+    @Override
+    public int hashCode(){ return Objects.hash(getName()); }
+}
