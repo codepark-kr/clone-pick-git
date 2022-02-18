9dd2b03a1aeaa97b1366b852928d4af62425580f
박예빈 <codepark.kr@gmail.com>
2022-02-18 13:20:29 +0900

    [STY] Change the wrong typo of class name
    - Change the wrong typo :
      NoYetCreatedPortfolioException ->
      NotYetCreatedPortfolioException

--- backend/pick-git/src/main/java/com/pg/pickgit/exception/portfolio/NotYetCreatedPortfolioException.java
+++ backend/pick-git/src/main/java/com/pg/pickgit/exception/portfolio/NotYetCreatedPortfolioException.java
@@ -1,16 +1,16 @@
-package com.pg.pickgit.portfolio;
+package com.pg.pickgit.exception.portfolio;
 
 import org.springframework.http.HttpStatus;
 
-public class NoYetCreatedPortfolioException extends PortfolioException{
+public class NotYetCreatedPortfolioException extends PortfolioException{
 
     private static final String ERROR_CODE = "R0006";
     private static final HttpStatus HTTP_STATUS = HttpStatus.NO_CONTENT;
     private static final String MESSAGE = "The portfolio doesn't generated yet.";
 
-    public NoYetCreatedPortfolioException(){ this(ERROR_CODE, HTTP_STATUS, MESSAGE); }
+    public NotYetCreatedPortfolioException(){ this(ERROR_CODE, HTTP_STATUS, MESSAGE); }
 
-    private NoYetCreatedPortfolioException(
+    private NotYetCreatedPortfolioException(
             String errorCode,
             HttpStatus httpStatus,
             String message
