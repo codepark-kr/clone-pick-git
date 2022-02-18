a90511943d554b3d2bcf61ee52a1d4e492722082
박예빈 <codepark.kr@gmail.com>
2022-02-18 11:36:36 +0900

    [ADD] Add new DTO for custom-exception
    - New add new DTO :
      ApiErrorResponse

---
+++ backend/pick-git/src/main/java/com/pg/pickgit/exception/dto/ApiErrorResponse.java
@@ -0,0 +1,12 @@
+package com.pg.pickgit.exception.dto;
+
+public class ApiErrorResponse {
+
+    private String errorCode;
+
+    private ApiErrorResponse(){}
+
+    public ApiErrorResponse(String errorCode){ this.errorCode = errorCode; }
+
+    public String getErrorCode(){ return errorCode; }
+}
