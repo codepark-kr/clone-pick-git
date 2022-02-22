d08f01c02b5a23b27ba14ef105b872e3afb96252
박예빈 <codepark.kr@gmail.com>
2022-02-22 18:11:02 +0900

    [ADD] Add new auth_interceptor_register/register_type
    - Newly Add new classes :
      AuthenticateStorageForRegisterType,
      IgnoreAuthenticateStorageForRegisterType,
      RegisterType,
      StorageForRegisterType

---
+++ backend/pick-git/src/main/java/com/pg/pickgit/config/auth_interceptor_register/register_type/AuthenticateStorageForRegisterType.java
@@ -0,0 +1,5 @@
+package com.pg.pickgit.config.auth_interceptor_register.register_type;
+
+public class AuthenticateStorageForRegisterType implements StorageRegisterType{
+
+}
---
+++ backend/pick-git/src/main/java/com/pg/pickgit/config/auth_interceptor_register/register_type/RegisterType.java
@@ -0,0 +1,6 @@
+package com.pg.pickgit.config.auth_interceptor_register.register_type;
+
+public enum RegisterType {
+    AUTHENTICATE,
+    IGNORE_AUTHENTICATE
+}
