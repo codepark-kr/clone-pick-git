6ba7ed66091cd12cd3c2bce3075908d1b0da0224
박예빈 <codepark.kr@gmail.com>
2022-02-24 17:28:58 +0900

    [ADD] Add new import-statements for newly-generated classes

--- backend/pick-git/src/main/java/com/pg/pickgit/exception/redis/EmbeddedRedisServerException.java
+++ backend/pick-git/src/main/java/com/pg/pickgit/exception/redis/EmbeddedRedisServerException.java
@@ -1,13 +1,13 @@
 package com.pg.pickgit.exception.redis;
 
-import com.pg.pickgit.exception.ApplicationException;
 import org.springframework.http.HttpStatus;
+import com.pg.pickgit.exception.ApplicationException;
 
 public class EmbeddedRedisServerException extends ApplicationException {
 
     private static final String ERROR_CODE = "V0002";
     private static final HttpStatus HTTP_STATUS = HttpStatus.INTERNAL_SERVER_ERROR;
-    private static final String MESSAGE = "Connect to Redis internal server has been failed.";
+    private static final String MESSAGE = "Connect to internal server of Redis has been failed.";
 
     public EmbeddedRedisServerException(){ this(ERROR_CODE, HTTP_STATUS, MESSAGE); }
 
