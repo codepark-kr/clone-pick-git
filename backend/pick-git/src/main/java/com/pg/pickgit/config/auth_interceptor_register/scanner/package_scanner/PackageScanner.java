94428e3ead0a783ad7f8349e24aea5ebeccd6773
박예빈 <codepark.kr@gmail.com>
2022-02-24 17:19:13 +0900

    [ADD] Add new configuration for package-scan
    - Newly add configuration for package -
      scan the `PackageScanner`

---
+++ backend/pick-git/src/main/java/com/pg/pickgit/config/auth_interceptor_register/scanner/package_scanner/PackageScanner.java
@@ -0,0 +1,27 @@
+package com.pg.pickgit.config.auth_interceptor_register.scanner.package_scanner;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class PackageScanner {
+
+    private final String basePackage;
+
+    public PackageScanner(String basePackage) { this.basePackage = basePackage; }
+
+    public List<String> getAllClassNames() {
+        Reflections reflections = new Reflections(
+                basePackage,
+                new SubTypesScanner(false)
+        );
+
+        return new HashSet<>(reflections.getSubTypesOf(Object.class)).stream()
+                .map(Class::getName)
+                .collect(toList());
+    }
+}
