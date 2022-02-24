96bc08c150d1089f7b88bb0b08d22ec571d4344c
박예빈 <codepark.kr@gmail.com>
2022-02-24 17:27:21 +0900

    [ADD] Add WebClientConfiguration

---
+++ backend/pick-git/src/main/java/com/pg/pickgit/config/WebClientConfiguration.java
@@ -0,0 +1,12 @@
+package com.pg.pickgit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfiguration {
+
+    @Bean
+    public WebClient webClient(){ return WebClient.create(); }
+}
